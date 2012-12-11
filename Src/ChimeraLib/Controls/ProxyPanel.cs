36997680b444ef0db5222cc890adc928b649882e
= <jm726@st-andrews.ac.uk>
2012-12-11 14:47:10 +0000

    Updated licenses.

--- Src/ChimeraLib/Controls/ProxyPanel.cs
+++ Src/ChimeraLib/Controls/ProxyPanel.cs
@@ -1,20 +1,19 @@
-﻿/*************************************************************************
-Copyright (c) 2012 John McCaffery 
+﻿/****Chimera (c) 2012 John McCaffery 
 
-This file is part of Armadillo SlaveProxy.
+This file is part of Chimera.
 
-Routing Project is free software: you can redistribute it and/or modify
+Chimera is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Routing Project is distributed in the hope that it will be useful,
+Chimera is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Routing Project.  If not, see <http://www.gnu.org/licenses/>.
+along with Chimera.  If not, see <http://www.gnu.org/licenses/>.
 
 **************************************************************************/
 
