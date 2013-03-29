f5a158aea4e8aaa2c9c272e25fb4d76439430762
= <=jm726@st-andrews.ac.uk>
2013-03-29 12:23:54 +0000

    Launcher implemented. CutTransition not implemented.

---
+++ Src/ChimeraLib/Overlay/Transitions/CutWindowTransition.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chimera.Interfaces.Overlay;
+using Chimera.Overlay;
+using System.Drawing;
+
+namespace Chimera.Overlay.Transitions {
+    public class CutWindowTransitionFactory : IWindowTransitionFactory {
+        public IWindowTransition Create(StateTransition transition, Window window) {
+            throw new NotImplementedException();
+        }
+    }
+    public class CutWindowTransition : IWindowTransition {
+        public event Action<IWindowTransition> Finished;
+
+        public Overlay.StateTransition StateTransition {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IWindowState To {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IWindowState From {
+            get { throw new NotImplementedException(); }
+        }
+
+        public WindowOverlayManager Manager {
+            get {
+                throw new NotImplementedException();
+            }
+            set {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Begin() {
+            throw new NotImplementedException();
+        }
+
+        public void Cancel() {
+            throw new NotImplementedException();
+        }
+
+        public bool NeedsRedrawn {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void RedrawStatic(Rectangle clip, Graphics graphics) {
+            throw new NotImplementedException();
+        }
+
+        public void DrawDynamic(Graphics graphics) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Src/LauncherLib/SimpleSingleInstanceLauncher.cs
+++ Src/LauncherLib/SimpleSingleInstanceLauncher.cs
@@ -12,6 +12,7 @@ using Chimera.OpenSim;
 using Chimera.Overlay.States;
 using Chimera.Overlay.Triggers;
 using Chimera.Overlay;
+using Chimera.Overlay.Transitions;
 
 namespace Chimera.Launcher {
     public class SimpleSingleInstanceLauncher {
@@ -49,7 +50,8 @@ namespace Chimera.Launcher {
             mKinect = new KinectInput(new IDeltaInput[] { timespan, dolphin }, new IHelpTrigger[] { trigger }, simpleFactory, pointFactory);
             //ISystemInput kinectDolphin = new DeltaBasedInput(dolphin);
 
-            Window[] windows = new Window[] { new Window("Main Window", output) };
+            Window mainWindow = new Window("Main Window", output);
+            Window[] windows = new Window[] { mainWindow };
             //ImageHoverTrigger mOverlay = new ImageHoverTrigger("../Select1.jpg", .1f, .1f, .3f, .3f);
             //From mState = new TestState();
             //MainMenuItem item1 = new MainMenuItem(mState, mOverlay);
@@ -59,16 +61,16 @@ namespace Chimera.Launcher {
             ImageBGState helpScreen = new ImageBGState("HelpScreen", mCoordinator.StateManager, "../Images/CathedralHelp.png");
 
             DialRenderer renderer = new DialRenderer();
-            //InvisibleHoverTrigger splashHelpTrigger = new InvisibleHoverTrigger(mGoInWorldRender, 155f / 1920, 220f / 1080, (555f - 155f) / 1920, (870f - 220f) / 1080);
-            InvisibleHoverTrigger splashHelpTrigger = new InvisibleHoverTrigger(mCoordinator.StateManager, renderer, 265f / 1920, 255f / 1080, (675f - 255f) / 1920, (900f - 255f) / 1080f);
+            //InvisibleHoverTrigger splashHelpTrigger = new InvisibleHoverTrigger(mainWindow.OverlayManager, renderer, 155f / 1920f, 220f / 1080f, (555f - 155f) / 1920f, (870f - 220f) / 1080);
+            InvisibleHoverTrigger splashHelpTrigger = new InvisibleHoverTrigger(mainWindow.OverlayManager, renderer, 265f / 1920f, 255f / 1080f, (675f - 255f) / 1920f, (900f - 255f) / 1080f);
 
-            //InvisibleHoverTrigger helpSplashTrigger = new InvisibleHoverTrigger(mCoordinator.StateManager, renderer, 70f / 1920, 65f / 1080, (490f - 70f) / 1920, (300f - 65f) / 1080);
-            InvisibleHoverTrigger helpSplashTrigger = new InvisibleHoverTrigger(mCoordinator.StateManager, renderer, 70f / 1920, 65f / 1080, (490f - 70f) / 1920, (300f - 65f) / 1080);
+            //InvisibleHoverTrigger helpSplashTrigger = new InvisibleHoverTrigger(mainWindow.OverlayManager, renderer, 70f / 1920f, 65f / 1080f, (490f - 70f) / 1920f, (300f - 65f) / 1080f);
+            InvisibleHoverTrigger helpSplashTrigger = new InvisibleHoverTrigger(mainWindow.OverlayManager, renderer, 70f / 1920f, 65f / 1080f, (490f - 70f) / 1920f, (300f - 65f) / 1080f);
 
-            //InvisibleHoverTrigger helpWorldTrigger = new InvisibleHoverTrigger(mCoordinator.StateManager, renderer, 55f / 1920, 515f / 1080, (330f - 55f) / 1920, (950f - 515f) / 1080);
-            //InvisibleHoverTrigger helpWorldTrigger = new InvisibleHoverTrigger(mCoordinator.StateManager, renderer, 60f / 1920, 520f / 1080, (335f - 60f) / 1920, (945f - 520f) / 1080);
+            //InvisibleHoverTrigger helpWorldTrigger = new InvisibleHoverTrigger(mainWindow.OverlayManager, renderer, 55f / 1920f, 515f / 1080f, (330f - 55f) / 1920f, (950f - 515f) / 1080f);
+            //InvisibleHoverTrigger helpWorldTrigger = new InvisibleHoverTrigger(mainWindow.OverlayManager, renderer, 60f / 1920f, 520f / 1080f, (335f - 60f) / 1920f, (945f - 520f) / 1080f);
 
-            CutStateTransitionFactory cutTransition = new CutStateTransitionFactory();
+            CutWindowTransitionFactory cutTransition = new CutWindowTransitionFactory();
             StateTransition splashHelpTransition = new StateTransition(mCoordinator.StateManager, splashScreen, helpScreen, helpSplashTrigger, cutTransition);
             StateTransition helpSplashTransition = new StateTransition(mCoordinator.StateManager, splashScreen, helpScreen, helpSplashTrigger, cutTransition);
 
