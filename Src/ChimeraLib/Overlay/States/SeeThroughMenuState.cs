b8f0cdeac2ade55e2dd68f40a5ad542ba06a4b32
= <=jm726@st-andrews.ac.uk>
2013-04-08 09:50:10 +0100

    Added stubs for Slideshow, Video and See through Menu states.

---
+++ Src/ChimeraLib/Overlay/States/SeeThroughMenuState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chimera.Interfaces.Overlay;
+
+namespace Chimera.Overlay.States {
+    public class SeeThroughMenuState : State {
+        public SeeThroughMenuState(string name, StateManager manager)
+            : base(name, manager) {
+        }
+
+        public override IWindowState CreateWindowState(Window window) {
+            throw new NotImplementedException();
+        }
+
+        public override void TransitionToStart() {
+            throw new NotImplementedException();
+        }
+
+        protected override void TransitionToFinish() {
+            throw new NotImplementedException();
+        }
+
+        protected override void TransitionFromStart() {
+            throw new NotImplementedException();
+        }
+
+        public override void TransitionFromFinish() {
+            throw new NotImplementedException();
+        }
+    }
+}
