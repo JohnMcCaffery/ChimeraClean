2403e68a8b6cfe448b515b1bfda4c969f0fde81d
= <=jm726@st-andrews.ac.uk>
2013-04-08 12:23:56 +0100

    Added CameraPositionTrigger.

---
+++ Src/ChimeraLib/Overlay/Triggers/CameraPositionTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chimera.Interfaces.Overlay;
+using System.Drawing;
+
+namespace Chimera.Overlay.Triggers {
+    public class CameraPositionTrigger : ITrigger {
+        private bool mActive;
+        private Rectangle mActiveArea;
+
+        public event Action Triggered;
+
+        public CameraPositionTrigger(Coordinator coordinator) {
+            coordinator.CameraUpdated += new Action<Coordinator,CameraUpdateEventArgs>(coordinator_CameraUpdated);
+        }
+
+        private void coordinator_CameraUpdated(Coordinator coordinator, CameraUpdateEventArgs args) {
+            if (mActive && Triggered != null && mActiveArea.Contains(new Point((int) args.position.X, (int) args.position.Y))) {
+                Triggered();
+            }
+        }
+
+        public bool Active {
+            get { return mActive; }
+            set { mActive = value; }
+        }
+    }
+}
