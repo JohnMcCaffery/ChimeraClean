6a75a8eb459c9b3dc6cd49cf7bd574905bad5ab2
= <=jm726@st-andrews.ac.uk>
2013-04-09 17:53:18 +0100

    Started on VideoState.

--- Src/ChimeraLib/Overlay/States/VideoState.cs
+++ Src/ChimeraLib/Overlay/States/VideoState.cs
@@ -6,8 +6,14 @@ using Chimera.Interfaces.Overlay;
 
 namespace Chimera.Overlay.States {
     public class VideoState : State {
-        public VideoState(string name, StateManager manager)
+        private string mVideo;
+        private string mPlayer = "C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc";
+        private string mArgs = "-f";
+
+        public VideoState(string name, StateManager manager, string video)
             : base(name, manager) {
+
+            mVideo = video;
         }
 
         public override IWindowState CreateWindowState(Window window) {
