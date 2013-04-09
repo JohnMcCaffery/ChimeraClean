0a5535df72748b5fe935634eb45b77cfb36037c4
= <jm726@st-andrews.ac.uk>
2013-04-09 12:02:02 +0100

    Added step functionality to flythrough state.

--- Src/FlythroughLib/Overlay/FlythroughState.cs
+++ Src/FlythroughLib/Overlay/FlythroughState.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using Chimera.Interfaces.Overlay;
 using Chimera.Overlay;
+using Chimera.Overlay.States;
 
 namespace Chimera.Flythrough.Overlay {
     public class FlythroughState : State {
         private Flythrough mInput;
+        private SlideshowWindow mSlideshow;
+        private IImageTransition mSlideshowTransition;
+        private string mSlideshowWindowName;
+        private string mSlideshowFolder;
         private string mFlythrough;
+        private bool mStepping;
 
         public FlythroughState(string name, StateManager manager, string flythrough)
             : base(name, manager) {
@@ -17,17 +23,51 @@ namespace Chimera.Flythrough.Overlay {
             mInput = manager.Coordinator.GetInput<Flythrough>();
         }
 
+        public FlythroughState(string name, StateManager manager, string flythrough, params ITrigger[] steps)
+            : this(name, manager, flythrough) {
+
+            mStepping = true;
+
+            foreach (var step in steps) {
+                step.Triggered += new Action(step_Triggered);
+                if (step is IDrawable)
+                    AddFeature(step as IDrawable);
+            }
+        }
+
+        public FlythroughState(string name, StateManager manager, string flythrough, string slideshowWindow, string slideshowFolder, IImageTransition slideshowTransition, params ITrigger[] steps)
+            : this(name, manager, flythrough, steps) {
+
+            mSlideshowWindowName = slideshowWindow;
+            mSlideshowFolder = slideshowFolder;
+            mSlideshowTransition = slideshowTransition;
+        }
+
+        void step_Triggered() {
+            mInput.Step();
+        }
+
         public override IWindowState CreateWindowState(Window window) {
-            return new FlythroughWindowState(window.OverlayManager);
+            if (window.Name.Equals(mSlideshowWindowName)) {
+                mSlideshow = new SlideshowWindow(window.OverlayManager, mSlideshowFolder, mSlideshowTransition);
+                return mSlideshow;
+            }
+            return new WindowState(window.OverlayManager);
         }
 
         protected override void TransitionToFinish() { }
 
         protected override void TransitionFromStart() { }
 
-        public override void TransitionToStart() {            mInput.Coordinator.EnableUpdates = true;
+        public override void TransitionToStart() {
+            mInput.Coordinator.EnableUpdates = true;
             mInput.Load(mFlythrough);
-            mInput.Loop = true;
+
+            if (mStepping)
+                mInput.AutoStep = false;
+            else
+                mInput.Loop = true;
+
             mInput.Play();
         }
 
