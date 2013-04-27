1a9d80747a2aa14a058a22dd7d2933bc13f71f3f
= <jm726@st-andrews.ac.uk>
2013-04-27 20:58:06 +0100

    Added abstract base axis class ConstrainedAxis.

---
+++ Src/ChimeraLib/GUI/Controls/Inputs/ConstrainedAxisPanel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Chimera.Inputs;
+
+namespace Chimera.GUI.Controls.Inputs {
+    public partial class ConstrainedAxisPanel : UserControl {
+        private ConstrainedAxis mConstrainedAxis;
+
+        public ConstrainedAxisPanel() {
+            InitializeComponent();
+        }
+
+        public ConstrainedAxisPanel(Chimera.Inputs.ConstrainedAxis constrainedAxis)
+            : this() {
+            mConstrainedAxis = constrainedAxis;
+        }
+    }
+}
---
+++ Src/ChimeraLib/Inputs/ConstrainedAxis.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chimera.Interfaces;
+using System.Windows.Forms;
+using Chimera.GUI.Controls.Inputs;
+
+namespace Chimera.Inputs {
+    public abstract class ConstrainedAxis : IAxis {
+        private AxisBinding mBinding = AxisBinding.None;
+        private ConstrainedAxisPanel mPanel;
+
+        private float mRaw = 0f;
+        private float mDelta = 0f;
+        private float mDeadzone = .1f;
+        private float mRange = .5f;
+        private float mGrace = .1f;
+        private float mScale = 1f;
+
+        public float Deadzone {
+            get { return mDeadzone; }
+            set { 
+                mDeadzone = value;
+                Recalculate();
+            }
+        }
+
+        public float Range {
+            get { return mRange; }
+            set { 
+                mRange = value;
+                Recalculate();
+            }
+        }
+
+        public float Grace {
+            get { return mGrace; }
+            set { 
+                mGrace = value;
+                Recalculate();
+            }
+        }
+
+        public float Scale {
+            get { return mScale; }
+            set { 
+                mScale = value;
+                Recalculate();
+            }
+        }
+
+        protected ConstrainedAxis() { }
+
+        protected ConstrainedAxis(float deadzone, float range, float grace, float scale) {
+            mDeadzone = deadzone;
+            mRange = range;
+            mGrace = grace;
+            mScale = scale;
+        }
+
+        protected void SetRawValue(float value) {
+            mRaw = value;
+            Recalculate();
+        }
+
+        private void Recalculate() {
+            float old = mDelta;
+            if (mDelta < mDeadzone || mDelta > mDeadzone + mRange + mGrace)
+                mDelta = 0f;
+            if (mDelta <= mDeadzone + mRange)
+                mDelta = ((mDelta - mDeadzone) / mRange) * mScale;
+            else
+                mDeadzone = mScale;
+
+            if (Changed != null && mDelta != old)
+                Changed();
+        }
+
+        #region IAxis Members
+
+        public event Action Changed;
+
+        public virtual UserControl ControlPanel {
+            get {
+                if (mPanel == null)
+                    mPanel = new ConstrainedAxisPanel(this);
+                return mPanel;
+            }
+        }
+
+        public float Delta {
+            get { return mDelta; }
+        }
+
+        public AxisBinding Binding {
+            get { return mBinding; }
+            set {
+                if (mBinding != value) {
+                    mBinding = value;
+                    if (Changed != null)
+                        Changed();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
