439c8a0a55a95293b5b4caa8a88c8ce9c555bf7d
= <jm726@st-andrews.ac.uk>
2013-04-28 21:39:43 +0100

    Create SplitAxis and its control panel.

---
+++ Src/ChimeraLib/GUI/Controls/Plugins/SplitAxisPanel.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Chimera.GUI.Controls.Plugins {
+    partial class SplitAxisPanel {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.positiveBox = new System.Windows.Forms.GroupBox();
+            this.negativeBox = new System.Windows.Forms.GroupBox();
+            this.positivePulldown = new System.Windows.Forms.ComboBox();
+            this.negativePulldown = new System.Windows.Forms.ComboBox();
+            this.positivePanel = new System.Windows.Forms.Panel();
+            this.negativePanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.positiveBox.SuspendLayout();
+            this.negativeBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.positiveBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.negativeBox);
+            this.splitContainer1.Size = new System.Drawing.Size(323, 130);
+            this.splitContainer1.SplitterDistance = 160;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // positiveBox
+            // 
+            this.positiveBox.Controls.Add(this.positivePanel);
+            this.positiveBox.Controls.Add(this.positivePulldown);
+            this.positiveBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.positiveBox.Location = new System.Drawing.Point(0, 0);
+            this.positiveBox.Name = "positiveBox";
+            this.positiveBox.Size = new System.Drawing.Size(160, 130);
+            this.positiveBox.TabIndex = 0;
+            this.positiveBox.TabStop = false;
+            this.positiveBox.Text = "Positive";
+            // 
+            // negativeBox
+            // 
+            this.negativeBox.Controls.Add(this.negativePanel);
+            this.negativeBox.Controls.Add(this.negativePulldown);
+            this.negativeBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.negativeBox.Location = new System.Drawing.Point(0, 0);
+            this.negativeBox.Name = "negativeBox";
+            this.negativeBox.Size = new System.Drawing.Size(159, 130);
+            this.negativeBox.TabIndex = 0;
+            this.negativeBox.TabStop = false;
+            this.negativeBox.Text = "Negative";
+            // 
+            // positivePulldown
+            // 
+            this.positivePulldown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.positivePulldown.DisplayMember = "Name";
+            this.positivePulldown.FormattingEnabled = true;
+            this.positivePulldown.Location = new System.Drawing.Point(6, 19);
+            this.positivePulldown.Name = "positivePulldown";
+            this.positivePulldown.Size = new System.Drawing.Size(148, 21);
+            this.positivePulldown.TabIndex = 0;
+            this.positivePulldown.SelectedIndexChanged += new System.EventHandler(this.positivePulldown_SelectedIndexChanged);
+            // 
+            // negativePulldown
+            // 
+            this.negativePulldown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.negativePulldown.DisplayMember = "Name";
+            this.negativePulldown.FormattingEnabled = true;
+            this.negativePulldown.Location = new System.Drawing.Point(6, 19);
+            this.negativePulldown.Name = "negativePulldown";
+            this.negativePulldown.Size = new System.Drawing.Size(148, 21);
+            this.negativePulldown.TabIndex = 1;
+            this.negativePulldown.SelectedIndexChanged += new System.EventHandler(this.negativePulldown_SelectedIndexChanged);
+            // 
+            // positivePanel
+            // 
+            this.positivePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.positivePanel.Location = new System.Drawing.Point(6, 46);
+            this.positivePanel.Name = "positivePanel";
+            this.positivePanel.Size = new System.Drawing.Size(148, 81);
+            this.positivePanel.TabIndex = 1;
+            // 
+            // negativePanel
+            // 
+            this.negativePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.negativePanel.Location = new System.Drawing.Point(6, 46);
+            this.negativePanel.Name = "negativePanel";
+            this.negativePanel.Size = new System.Drawing.Size(148, 81);
+            this.negativePanel.TabIndex = 2;
+            // 
+            // SplitAxisPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "SplitAxisPanel";
+            this.Size = new System.Drawing.Size(323, 130);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.positiveBox.ResumeLayout(false);
+            this.negativeBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox positiveBox;
+        private System.Windows.Forms.GroupBox negativeBox;
+        private System.Windows.Forms.ComboBox positivePulldown;
+        private System.Windows.Forms.ComboBox negativePulldown;
+        private System.Windows.Forms.Panel positivePanel;
+        private System.Windows.Forms.Panel negativePanel;
+    }
+}
--- Src/ChimeraLib/Plugins/SplitAxis.cs
+++ Src/ChimeraLib/Plugins/SplitAxis.cs
@@ -6,9 +6,65 @@ using Chimera.Interfaces;
 
 namespace Chimera.Plugins {
     public class SplitAxis : IAxis {
+        private readonly List<IAxis> mAxes = new List<IAxis>();
+        private readonly string mName;
+
+        private AxisBinding mBinding;
         private IAxis mPositive;
         private IAxis mNegative;
 
+        public event Action<IAxis> AxisAdded;
+
+        public IEnumerable<IAxis> Axes { 
+            get { return mAxes; } 
+        }
+
+        public IAxis Positive {
+            get { return mPositive; }
+            set { mPositive = value; }
+        }
+
+        public IAxis Negative {
+            get { return mNegative; }
+            set { mNegative = value; }
+        }
+
+        public void AddAxis(IAxis axis) {
+            if (mPositive == null)
+                mPositive = axis;
+            else if (mNegative == null)
+                mNegative = null;
+
+            mAxes.Add(axis);
+
+            if (AxisAdded != null)
+                AxisAdded(axis);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="axes">First access will be positive, second negative.</param>
+        public SplitAxis(string name, params IAxis[] axes) {
+            mName = name;
+
+            foreach (var axis in axes)
+                AddAxis(axis);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="binding"></param>
+        /// <param name="axes">First access will be positive, second negative.</param>
+        public SplitAxis(string name, AxisBinding binding, params IAxis[] axes) {
+            mBinding = binding;
+
+            foreach (var axis in axes)
+                AddAxis(axis);
+        }
 
 
         #region IAxis Members
@@ -18,20 +74,16 @@ namespace Chimera.Plugins {
         }
 
         public float Delta {
-            get { throw new NotImplementedException(); }
+            get { return Math.Abs(mPositive.Delta) + (Math.Abs(mNegative.Delta) * -1f); }
         }
 
         public AxisBinding Binding {
-            get {
-                throw new NotImplementedException();
-            }
-            set {
-                throw new NotImplementedException();
-            }
+            get { return mBinding; }
+            set { mBinding = value; }
         }
 
         public string Name {
-            get { throw new NotImplementedException(); }
+            get { return mName; }
         }
 
         #endregion
