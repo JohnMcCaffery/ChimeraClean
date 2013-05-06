4dedbcf92702e4e0affb6b11ba64106eec1a8618
= <=jm726@st-andrews.ac.uk>
2013-05-06 13:19:46 +0100

    Added labels to touchscreen overlay.

--- Src/TouchscreenLib/GUI/TouchscreenForm.cs
+++ Src/TouchscreenLib/GUI/TouchscreenForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Chimera.Interfaces;
 
 namespace Touchscreen.GUI {
     public partial class TouchscreenForm : Form {
@@ -32,67 +33,89 @@ namespace Touchscreen.GUI {
             TouchscreenForm_Paint(e.Graphics, e.ClipRectangle, mPlugin);
         }
 
+        private static string GetBinding(AxisBinding binding) {
+            switch (binding) {
+                case AxisBinding.X: return "Move";
+                case AxisBinding.Pitch: return "Camera";
+                case AxisBinding.Z: return "Fly";
+            }
+            return "Unknown";
+        }
+
         internal static void TouchscreenForm_Paint(Graphics g, Rectangle clip, TouchscreenPlugin plugin) {
             int w = clip.Width;
             int h = clip.Height;
-            Rectangle rect = new Rectangle(
-                (int)(w * (plugin.Left.StartH + plugin.Left.PaddingH)),
-                (int)(h * plugin.Left.PaddingV),
-                (int)(w * plugin.Left.W),
-                (int)(h * plugin.Left.H));
-            g.DrawRectangle(Pens.Black, rect);
-            
-            int centreX = (rect.Width / 2) + rect.X;
-            int centreY = (rect.Height / 2) + rect.Y;
-            rect = new Rectangle(
-                (int) (centreX - (w * plugin.LeftX.Deadzone.Value)),
-                (int) (centreY - (h * plugin.LeftY.Deadzone.Value)),
-                (int) (w * plugin.LeftX.Deadzone.Value * 2),
-                (int) (h * plugin.LeftY.Deadzone.Value * 2));
-            g.DrawRectangle(Pens.Black, rect);
+            using (Font font = new Font("Verdana", 30f)) {
+
+                Rectangle rect = new Rectangle(
+                    (int)(w * (plugin.Left.StartH + plugin.Left.PaddingH)),
+                    (int)(h * plugin.Left.PaddingV),
+                    (int)(w * plugin.Left.W),
+                    (int)(h * plugin.Left.H));
+                g.DrawRectangle(Pens.Black, rect);
+
+                string msg = GetBinding(plugin.LeftY.Binding);
+                int textW = (int)g.MeasureString(msg, font).Width;
+                g.DrawString(msg, font, Brushes.Black, new Point(rect.X + ((rect.Width / 2) - (textW / 2)), rect.Y));
+
+
+                int centreX = (rect.Width / 2) + rect.X;
+                int centreY = (rect.Height / 2) + rect.Y;
+                rect = new Rectangle(
+                    (int)(centreX - (w * plugin.LeftX.Deadzone.Value)),
+                    (int)(centreY - (h * plugin.LeftY.Deadzone.Value)),
+                    (int)(w * plugin.LeftX.Deadzone.Value * 2),
+                    (int)(h * plugin.LeftY.Deadzone.Value * 2));
+                g.DrawRectangle(Pens.Black, rect);
 
 
 
 
-            rect = new Rectangle(
-                (int)(w * (plugin.Right.StartH + plugin.Right.PaddingH)),
-                (int)(h * plugin.Right.PaddingV),
-                (int)(w * plugin.Right.W),
-                (int)(h * plugin.Right.H));
-            g.DrawRectangle(Pens.Black, rect);
-            
-            centreX = (rect.Width / 2) + rect.X;
-            centreY = (rect.Height / 2) + rect.Y;
-            rect = new Rectangle(
-                (int) (centreX - (w * plugin.RightX.Deadzone.Value)),
-                (int) (centreY - (h * plugin.RightY.Deadzone.Value)),
-                (int) (w * plugin.RightX.Deadzone.Value * 2),
-                (int) (h * plugin.RightY.Deadzone.Value * 2));
-            g.DrawRectangle(Pens.Black, rect);
+                rect = new Rectangle(
+                    (int)(w * (plugin.Right.StartH + plugin.Right.PaddingH)),
+                    (int)(h * plugin.Right.PaddingV),
+                    (int)(w * plugin.Right.W),
+                    (int)(h * plugin.Right.H));
+                g.DrawRectangle(Pens.Black, rect);
+
+                msg = GetBinding(plugin.RightY.Binding);
+                textW = (int)g.MeasureString(msg, font).Width;
+                g.DrawString(msg, font, Brushes.Black, new Point(rect.X + ((rect.Width / 2) - (textW / 2)), rect.Y));
+
+                centreX = (rect.Width / 2) + rect.X;
+                centreY = (rect.Height / 2) + rect.Y;
+                rect = new Rectangle(
+                    (int)(centreX - (w * plugin.RightX.Deadzone.Value)),
+                    (int)(centreY - (h * plugin.RightY.Deadzone.Value)),
+                    (int)(w * plugin.RightX.Deadzone.Value * 2),
+                    (int)(h * plugin.RightY.Deadzone.Value * 2));
+                g.DrawRectangle(Pens.Black, rect);
 
 
 
 
-                
-
-            rect = new Rectangle(
-                (int)(w * (plugin.Single.StartH + plugin.Left.PaddingH)),
-                (int)(h * plugin.Single.PaddingV),
-                (int)(w * plugin.Single.W),
-                (int)(h * plugin.Single.H));
-            g.DrawRectangle(Pens.Black, rect);
-            
-            centreX = (rect.Width / 2) + rect.X;
-            centreY = (rect.Height / 2) + rect.Y;
-            rect = new Rectangle(
-                rect.X,
-                (int) (centreY - (h * plugin.Single.Deadzone.Value)),
-                rect.Width,
-                (int) (h * plugin.Single.Deadzone.Value * 2));
-            g.DrawRectangle(Pens.Black, rect);
 
 
+                rect = new Rectangle(
+                    (int)(w * (plugin.Single.StartH + plugin.Left.PaddingH)),
+                    (int)(h * plugin.Single.PaddingV),
+                    (int)(w * plugin.Single.W),
+                    (int)(h * plugin.Single.H));
+                g.DrawRectangle(Pens.Black, rect);
 
+                msg = GetBinding(plugin.Single.Binding);
+                textW = (int)g.MeasureString(msg, font).Width;
+                g.DrawString(msg, font, Brushes.Black, new Point(rect.X + ((rect.Width / 2) - (textW / 2)), rect.Y));
+
+                centreX = (rect.Width / 2) + rect.X;
+                centreY = (rect.Height / 2) + rect.Y;
+                rect = new Rectangle(
+                    rect.X,
+                    (int)(centreY - (h * plugin.Single.Deadzone.Value)),
+                    rect.Width,
+                    (int)(h * plugin.Single.Deadzone.Value * 2));
+                g.DrawRectangle(Pens.Black, rect);
+            }
         }
     }
 }
