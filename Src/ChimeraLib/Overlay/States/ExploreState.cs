bf497ff441dbe6738261a25165d892d8d6a7d2d3
= <jm726@st-andrews.ac.uk>
2013-05-13 23:01:44 +0100

    Added Explore State.

---
+++ Src/ChimeraLib/Overlay/States/ExploreState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chimera.Interfaces.Overlay;
+
+namespace Chimera.Overlay.States {
+    public class ExploreState : State {
+        public ExploreState(string name, StateManager manager)
+            : base(name, manager) {
+        }
+
+        public override IWindowState CreateWindowState(Window window) {
+            return new WindowState(window.OverlayManager);
+        }
+
+        public override void TransitionToStart() {
+            Manager.Coordinator.EnableUpdates = true;
+        }
+
+        protected override void TransitionToFinish() { }
+
+        protected override void TransitionFromStart() { }
+
+        public override void TransitionFromFinish() { }
+    }
+}
