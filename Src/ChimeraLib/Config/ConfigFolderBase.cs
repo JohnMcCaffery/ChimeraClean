57b18d7c445ddf16c03027e37f03e73f9e920482
= <=jm726@st-andrews.ac.uk>
2013-06-25 13:16:06 +0100

    Added common config folder.

--- Src/ChimeraLib/Config/ConfigFolderBase.cs
+++ Src/ChimeraLib/Config/ConfigFolderBase.cs
@@ -7,7 +7,8 @@ using System.IO;
 
 namespace Chimera.Config {
     public abstract class ConfigFolderBase : ConfigBase {
-        private const string DEFAULT_FOLDER = "../Config";
+        private const string DEFAULT_FOLDER = "../Configs/Config";
+        private const string DEFAULT_COMMON_FOLDER = "../Configs/Common";
 
         private static string GetFile(string group, string[] args) {
             IConfigSource source = GetMainConfig(args);
@@ -17,8 +18,17 @@ namespace Chimera.Config {
 
             string folder = cfg.Get("ConfigFolder", DEFAULT_FOLDER);
             string file = cfg.Get(group, group + ".ini");
-            return Path.GetFullPath(Path.Combine(folder, file));
+            file = Path.GetFullPath(Path.Combine(folder, file));
+
+            if (!File.Exists(file)) {
+                folder = cfg.Get("ConfigFolderCommon", DEFAULT_COMMON_FOLDER);
+                file = cfg.Get(group, group + ".ini");
+                file = Path.GetFullPath(Path.Combine(folder, file));
+            }
+
+            return file;
         }
+
         protected ConfigFolderBase(string group, params string[] args) :
             this(group, group, args) {
         }
