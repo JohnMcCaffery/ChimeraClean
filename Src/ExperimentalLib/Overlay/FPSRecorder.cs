5f60ad9a7788b797f39bae6d09384ad9584cc605
= <=jm726@st-andrews.ac.uk>
2013-10-16 17:09:45 +0100

    Added FPSRecorder state.

---
+++ Src/ExperimentalLib/Overlay/FPSRecorder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chimera.Overlay;
+using Chimera.Interfaces.Overlay;
+
+namespace Chimera.Experimental.Overlay {
+    public class FPSRecorder : State {
+
+        protected override void TransitionToStart() {
+            throw new NotImplementedException();
+        }
+
+        protected override void TransitionToFinish() {
+            throw new NotImplementedException();
+        }
+
+        protected override void TransitionFromStart() {
+            throw new NotImplementedException();
+        }
+
+        protected override void TransitionFromFinish() {
+            throw new NotImplementedException();
+        }
+    }
+}
