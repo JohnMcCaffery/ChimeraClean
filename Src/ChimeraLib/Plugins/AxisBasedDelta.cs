cae88b01e6d42daf4827f287fa8c3cc3f2c4f8fb
= <=jm726@st-andrews.ac.uk>
2014-04-16 19:50:18 +0100

    Fixed AxisBasedDelta to load the correct file.

--- Src/ChimeraLib/Plugins/AxisBasedDelta.cs
+++ Src/ChimeraLib/Plugins/AxisBasedDelta.cs
@@ -107,7 +107,7 @@ namespace Chimera.Plugins {
         public AxisBasedDelta(string name, params IAxis[] axes) {
             Logger = LogManager.GetLogger(name);
             mName = name;
-            mConfig = new AxisConfig(name + "Movement");
+            mConfig = new AxisConfig(name);
 
             mTickListener = new Action(mCore_Tick);
 
