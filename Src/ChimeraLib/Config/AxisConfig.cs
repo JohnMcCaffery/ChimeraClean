88cb2010bc33a4f306f8d2a3f9593a2cf38f3bc5
= <=jm726@st-andrews.ac.uk>
2014-04-16 11:19:14 +0100

    Separated AxisConfig into its own file and added support for configuring axes into config.

---
+++ Src/ChimeraLib/Config/AxisConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chimera.Interfaces;
+using Chimera.Plugins;
+
+namespace Chimera.Config {
+    public class AxisConfig : ConfigFolderBase {
+        private string mType;
+
+        public AxisConfig(string type)
+            : base(type, new string[0]) {
+            mType = type;
+        }
+
+        public override string Group {
+            get { return mType + "Movement"; }
+        }
+
+        protected override void InitConfig() { }
+
+        public float GetDeadzone(string name) {
+            return Get("Deadzones", name, .1f, "");
+        }
+
+        public float GetScale(string name) {
+            return Get("Scales", name, 1f, "");
+        }
+
+        public AxisBinding GetBinding(string name) {
+            return (AxisBinding)Enum.Parse(typeof(AxisBinding), Get("Bindings", name, "None", ""));
+        }
+
+        public void ConfigureAxis(IAxis axis, Core core) {
+            if (core != null && axis is ITickListener)
+                (axis as ITickListener).Init(core);
+
+            if (axis is ConstrainedAxis) {
+                ConstrainedAxis ax = axis as ConstrainedAxis;
+                ax.Deadzone.Value = GetDeadzone(axis.Name);
+                ax.Scale.Value  = GetScale(axis.Name);
+            }
+            if (axis.Binding == AxisBinding.NotSet)
+                axis.Binding = GetBinding(axis.Name);
+        }
+ 
+    }
+}
