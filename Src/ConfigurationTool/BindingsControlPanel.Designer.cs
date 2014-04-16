114802127e8b1d26cf47979b0c36ac194d2db5b4
= <=jm726@st-andrews.ac.uk>
2014-04-16 18:22:16 +0100

    Added confguration tool application.

---
+++ Src/ConfigurationTool/BindingsControlPanel.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ConfigurationTool {
+    partial class BindingsControlPanel {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.BoundCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.InterfaceCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LibraryCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DiscriptionCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.loadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.BoundCol,
+            this.InterfaceCol,
+            this.NameCol,
+            this.LibraryCol,
+            this.DiscriptionCol});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(695, 341);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // BoundCol
+            // 
+            this.BoundCol.Text = "Bound";
+            // 
+            // InterfaceCol
+            // 
+            this.InterfaceCol.Text = "Interface";
+            // 
+            // NameCol
+            // 
+            this.NameCol.Text = "Name";
+            // 
+            // LibraryCol
+            // 
+            this.LibraryCol.Text = "Library";
+            // 
+            // DiscriptionCol
+            // 
+            this.DiscriptionCol.Text = "Description";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadButton.Location = new System.Drawing.Point(617, 3);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // BindingsControlPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.listView1);
+            this.Name = "BindingsControlPanel";
+            this.Size = new System.Drawing.Size(695, 341);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader BoundCol;
+        private System.Windows.Forms.ColumnHeader InterfaceCol;
+        private System.Windows.Forms.ColumnHeader NameCol;
+        private System.Windows.Forms.ColumnHeader LibraryCol;
+        private System.Windows.Forms.ColumnHeader DiscriptionCol;
+        private System.Windows.Forms.Button loadButton;
+    }
+}
---
+++ Src/ConfigurationTool/ConfigurationTool.Designer.cs
@@ -0,0 +1,54 @@
+﻿using ConfigurationTool;
+namespace Chimera.ConfigurationTool {
+    partial class ConfigurationTool {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.bindingsControlPanel1 = new BindingsControlPanel();
+            this.SuspendLayout();
+            // 
+            // bindingsControlPanel1
+            // 
+            this.bindingsControlPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bindingsControlPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bindingsControlPanel1.Name = "bindingsControlPanel1";
+            this.bindingsControlPanel1.Size = new System.Drawing.Size(284, 262);
+            this.bindingsControlPanel1.TabIndex = 0;
+            // 
+            // ConfigurationTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.bindingsControlPanel1);
+            this.Name = "ConfigurationTool";
+            this.Text = "Chimera Configuration Tool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private global::ConfigurationTool.BindingsControlPanel bindingsControlPanel1;
+    }
+}
