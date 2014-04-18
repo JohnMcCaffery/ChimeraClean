c24fbecbc7f04bc0b7ef010d1d4c42c6588d0a0b
= <=jm726@st-andrews.ac.uk>
2014-04-18 11:03:28 +0100

    Changing up how config loading works.

--- Src/OverlayLib/Plugin/OverlayConfig.cs
+++ Src/OverlayLib/Plugin/OverlayConfig.cs
@@ -27,18 +27,18 @@ namespace Chimera.Overlay {
             : base("Overlay", args) { }
 
         protected override void InitConfig() {
-            Launcher = Get("Launcher", "Launcher.MinimumLauncher", "The launcher which will run chimera.");
+            Launcher = GetStr("Launcher", "Launcher.MinimumLauncher", "The launcher which will run chimera.");
 
             Fullscreen = Get("Fullscreen", true, "Whether overlay windows should start full screen.");
             LaunchOverlay = Get("LaunchOverlay", true, "Whether to create the overlay windows when the system starts up.");
             AlwaysOnTop = Get("AlwaysOnTop", true, "Whether to force overlay windows to always be on top.");
             ControlPointer = Get("ControlPointer", true, "Whether to allow the overlay to move the system pointer.");
             DisablePointer = Get("DisablePointer", true, "Wether to move the pointer off the screen.");
-            InterfaceMode = Get("InterfaceMode", "HoverBased", "The mode the overlay is in. Use this to choose between different configs embedded in the same Overlay config file.");
-            OverlayFile = Get("OverlayFile", null, "The overlay file to load.");
+            InterfaceMode = GetStr("InterfaceMode", "HoverBased", "The mode the overlay is in. Use this to choose between different configs embedded in the same Overlay config file.");
+            OverlayFile = GetFile("OverlayFile", null, "The overlay file to load.");
             IdleEnabled = Get("IdleEnabled", true, "Whether the idle triggers should be activated.");
 
-            DefaultCursor = Get("DefaultCursor", "Cursors/cursor.cur", "The default cursor file to use on the overlay as the overlay cursor.");
+            DefaultCursor = GetFile("DefaultCursor", "Cursors/cursor.cur", "The default cursor file to use on the overlay as the overlay cursor.");
         }
     }
 }
