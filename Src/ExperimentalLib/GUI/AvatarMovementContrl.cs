55103525b1ac7c605e082e645c095412f6bbe030
= <jm726@st-andrews.ac.uk>
2014-04-19 17:04:11 +0100

    Started creating plugin that can move avatars.

---
+++ Src/ExperimentalLib/GUI/AvatarMovementContrl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Chimera.Experimental.Plugins;
+
+namespace Chimera.Experimental.GUI {
+    public partial class AvatarMovementContrl : UserControl {
+        private AvatarMovementPlugin mPlugin;
+
+        public AvatarMovementContrl() {
+            InitializeComponent();
+        }
+
+        public AvatarMovementContrl(AvatarMovementPlugin plugin) : this() {
+            mPlugin = plugin;
+        }
+
+        private void startButton_Click(object sender, EventArgs e) {
+            mPlugin.Start();
+        }
+    }
+}
