6f8f392acaffe56019a9ffccbc1bb1bbdc12489d
= <=jm726@st-andrews.ac.uk>
2014-05-27 14:20:40 +0100

    Added the ability to specify an exit code.

--- Src/ChimeraLib/GUI/Forms/CoordinatorForm.Designer.cs
+++ Src/ChimeraLib/GUI/Forms/CoordinatorForm.Designer.cs
@@ -524,6 +524,7 @@ namespace Chimera.GUI.Forms {
             this.ClientSize = new System.Drawing.Size(911, 822);
             this.Controls.Add(this.hSplit);
             this.Name = "CoordinatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Caen Control Panel";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CoordinatorForm_FormClosing);
