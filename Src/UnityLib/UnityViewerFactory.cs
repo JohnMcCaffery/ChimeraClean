a444f3212b320d83ef5524d14ff1cdd07b943334
780 Shuttle <jm726@st-andrews.ac.uk>
2015-03-10 14:53:07 +0000

    Add the Unity state.

---
+++ Src/UnityLib/UnityViewerFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chimera.Interfaces;
+
+namespace Chimera.OpenSim
+{
+    public class UnityViewerFactory : IOutputFactory
+    {
+        private bool mPluginAssigned;
+
+        public UnityViewerFactory(IEnumerable<ISystemPlugin> plugins)
+        {
+            
+        }
+
+        public IOutput Create()
+        {
+            return null;
+        }
+    }
+}
