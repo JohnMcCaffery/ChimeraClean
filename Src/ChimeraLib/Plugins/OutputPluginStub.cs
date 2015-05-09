d4f6518fb8ae7be163ae468a1665298259e49b95
John McCaffery <jm726@st-andrews.ac.uk>
2015-05-09 12:17:12 +0100

    Added output plugin stub.

---
+++ Src/ChimeraLib/Plugins/OutputPluginStub.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chimera.Plugins
+{
+    public class OutputPluginStub : IOutput {
+        private Frame mFrame;
+
+        public Frame Frame {
+            get { return mFrame; }
+        }
+
+        public Control ControlPanel {
+            get { return new Control(); }
+        }
+
+        public bool AutoRestart {
+            get { return false; }
+            set { }
+        }
+
+        public string State {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Type {
+            get { return "Stub"; }
+        }
+
+        public Fill Fill {
+            get {
+                return Chimera.Fill.Windowed;
+            }
+            set { }
+        }
+
+        public bool Active {
+            get { return false; }
+        }
+
+        public System.Diagnostics.Process Process {
+            get { return null; }
+        }
+
+        public void Init(Frame frame) {
+        }
+
+        public bool Launch() {
+            return false;
+        }
+
+        public void Close() {
+        }
+
+        public void Restart(string reason) {
+        }
+    }
+}
